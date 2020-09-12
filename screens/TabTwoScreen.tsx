61b31002a1d8198f676e37f2a9f9d7858db7de62
dev86k <example@example.com>
2020-09-12 23:08:59 +0900

    add

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,Image } from 'react-native';
+import logo from '../assets/logo.png';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
