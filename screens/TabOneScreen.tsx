8344b275f40138fe5caef7347dccb5bc66ec7e47
dev86k <example@example.com>
2020-09-16 23:16:29 +0900

    listview

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,51 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image, FlatList, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+const ITEM_WIDTH = Dimensions.get('window').width;
+//import EditScreenInfo from '../components/EditScreenInfo';
+//import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  state = {
+  list: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+}
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+          <View style={styles.container}>
+            <FlatList
+            data={this.state.list}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={2}
+
+            renderItem={({item}) => (
+              <View>
+               <Image
+                source={{ uri: 'https://source.unsplash.com/random' }}
+                style={styles.imageStyle}
+                />
+                <Text style={styles.textStyle}>test</Text>
+              </View>
+            )}
+            />
+          </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  imageStyle: {
+    width: ITEM_WIDTH / 2,
+    height: ITEM_WIDTH / 3,
+    margin: 1,
+    resizeMode: 'cover',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  textStyle: {
+    marginBottom: 10,
   },
 });
