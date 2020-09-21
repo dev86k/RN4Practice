7ca32f523488363a1dcf26163993c8ec6bf9fdf1
dev86k <example@example.com>
2020-09-22 00:17:46 +0900

    add FlatList

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text,StyleSheet, Image, FlatList, Dimensions } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer,DrawerActions } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { createStackNavigator} from '@react-navigation/stack';
 import { Ionicons, AntDesign } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+const ITEM_WIDTH = Dimensions.get('window').width;
 const Tab = createBottomTabNavigator();
 function HomeScreen() {
   return (
@@ -52,8 +54,15 @@ function TabAScreen({navigation}) {
       <Stack.Screen name="TabA Home" component={TabADetailsScreen}
       options={{
           headerTitle: "TabA",
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
           headerRight: (props) => (
-                   <AntDesign name="bars" size={26} color="black" style={{ marginRight: 10 }}
+                   <AntDesign name="bars" size={26} color="white" style={{ marginRight: 10 }}
                      {...props}
                        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
                    />
@@ -64,6 +73,29 @@ function TabAScreen({navigation}) {
   );
 }
 function TabADetailsScreen({navigation}) {
+state = {
+list: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+}
+return (
+        <View style={styles.container}>
+          <FlatList
+          data={state.list}
+          keyExtractor={(item, index) => index.toString()}
+          numColumns={2}
+
+          renderItem={({item}) => (
+            <View>
+             <Image
+              source={{ uri: 'https://source.unsplash.com/random?'+item }}
+              style={styles.imageStyle}
+              />
+              <Text style={styles.textStyle}>test</Text>
+            </View>
+          )}
+          />
+        </View>
+);
+/*
   return (
     <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
       <Text>
@@ -75,6 +107,7 @@ function TabADetailsScreen({navigation}) {
       />
     </View>
   );
+  */
 }
 function Details() {
   return (
@@ -106,3 +139,22 @@ export default function App() {
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  imageStyle: {
+    width: ITEM_WIDTH / 2,
+    height: ITEM_WIDTH / 3,
+    margin: 1,
+    resizeMode: 'cover',
+  },
+  textStyle: {
+    marginBottom: 10,
+  },
+});
